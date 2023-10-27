479ad2036a6d3fbbfb7b81c082b162f3b8a9753a
Erik Prakoso <erikprakoso1993@gmail.com>
2023-10-27 20:43:54 +0700

    update config

--- config/db.config.js
+++ config/db.config.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 module.exports = {
-    url: "mongodb+srv://admin:gduzJ5WDelsdLpNb@cluster0.5msvfj0.mongodb.net/hpos-db?retryWrites=true&w=majority"
+    url: process.env.URL_MONGO_DB
 }
